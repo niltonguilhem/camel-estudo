47142f68bcb9de72bef8ecefd26c194f83ea6389
niltonguilhem <nil.guilhem@gmail.com>
2023-03-24 14:22:13 -0300

    marshal and setHeader

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -14,8 +14,11 @@ public class RotaPedidos {
 			@Override
 			public void configure() throws Exception {
 
-				from("file:pedidos?delay=5s").
+				from("file:pedidos?delay=5s&noop=true").
 						log("${id}").
+						marshal().xmljson().
+						log("${body}").
+						setHeader("CamelFileName", simple("${file:name.noext}.json")).
 						to("file:saida");
 
 			}
